a0639cb95ad7f80b9483e4ac3238017a1dd7f257
JackLi-hin <s2077992@ed.ac.uk>
2023-02-14 17:54:48 +0000

    command change

--- bridge/src/server.ts
+++ bridge/src/server.ts
@@ -1,2 +1,20 @@
-console.log('Hello from Bridge');
+import {TopicServer} from "webtopics";
+import { Server } from "socket.io";
+import { createPresetCommandService } from "./Channels/presetCommandService";
+import { CreatePresetCommand } from "./command/StageCommand/CreatePresetCommand";
+import { Context } from "./controller/Context";
+import { TopicClient } from "webtopics";
+import { preset } from "../../schema/src/stage/stageState";
+const dummyCreatePresetClient =  new TopicClient("http://localhost:3000");
+const socketIOServer = new Server(3000);
+const topicServer = new TopicServer(socketIOServer);
+const context = new Context();
+const dummyPreset :preset= {name:"preset1",
+}
+topicServer.srv(createPresetCommandService, (preset) => {
+    const cP = new CreatePresetCommand(preset)
+    return cP.execute(context)
+}
+dummyCreatePresetClient.req(createPresetCommandService,)
 
+)

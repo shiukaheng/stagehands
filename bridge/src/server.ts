63734694b58a4d567e0328144296e5402e29b6ca
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-02-21 18:16:35 +0000

    fixed imports

--- bridge/src/server.ts
+++ bridge/src/server.ts
@@ -5,8 +5,9 @@ import { CreatePresetCommand } from "./command/StageServiceCommand/CreatePresetC
 import { Context } from "./controller/Context";
 import { TopicClient } from "webtopics";
 import { preset } from "../../schema";
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 import { Controller } from "./controller/Controller";
+
 const dummyCreatePresetSocket=io("http://localhost:3000");
 const dummyCreatePresetClient =  new TopicClient(dummyCreatePresetSocket);
 const socketIOServer = new Server(3000);
@@ -17,6 +18,7 @@ const dummyPreset :preset= {
     name:"preset1",
     poses:[]
 }
+
 topicServer.srv(createPresetCommandService, (preset) => {
     const cP = new CreatePresetCommand(preset)
     return cP.execute(context)

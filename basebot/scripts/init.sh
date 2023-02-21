40775e88e13109639286959bbdcc8299b56dd9f5
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-02-21 02:36:15 +0000

    updated packages

--- basebot/scripts/init.sh
+++ basebot/scripts/init.sh
@@ -20,3 +20,8 @@ export TURTLEBOT3_MODEL=waffle_pi
 # export ROS_MASTER_URI=http://192.168.105.158:11311
 export ROS_MASTER_URI=http://localhost:11311
 export ROS_HOSTNAME=$HOSTNAME
+
+function build {
+    catkin build
+    source /catkin_ws/devel/setup.bash
+}

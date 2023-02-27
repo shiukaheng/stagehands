1baeb106e010cb75019f75983d55febb0271cd82
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-02-27 11:24:11 +0000

    changes to fake_server

--- fake_bridge/src/server.ts
+++ fake_bridge/src/server.ts
@@ -8,17 +8,26 @@ export class FakeBridgeServer {
     private io: Server
     private fleetState: FleetState = {
         "bot1": {
-            name: "bot1",
-            position: [0, 0, 0],
+            name: "Alice",
+            position: [1, 0, 0],
             rotation: [0, 0, 0, 1]
         },
         "bot2": {
-            name: "bot2",
-            position: [0, 0, 0],
+            name: "Bob",
+            position: [-1, 0, 0],
             rotation: [0, 0, 0, 1]
         }
     }
-    private presetsState: Presets = {}
+    private presetsState: Presets = {
+        "preset1": {
+            name: "Presets 1",
+            bots: {}
+        },
+        "preset2": {
+            name: "Presets 2",
+            bots: {}
+        }
+    }
 
     /**
      * Constructor for FakeBridgeServer
@@ -30,9 +39,12 @@ export class FakeBridgeServer {
                 origin: "*",
             }
         })
-        this.topicServer = new TopicServer(this.io)
-        this.topicServer.pub(liveFleetChannel, this.fleetState)
-        this.topicServer.pub(presetsChannel, this.presetsState)
+        this.topicServer = new TopicServer(this.io, {logTopics: true})
         console.log(`✅ Fake bridge server running on port ${port}`)
+        setInterval(()=>{
+            this.topicServer.pub(liveFleetChannel, this.fleetState)
+            this.topicServer.pub(presetsChannel, this.presetsState)
+        }, 1000)
+        
     }
 }

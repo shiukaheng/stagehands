483310d30d05c379b2edfb5a232b8a79d063a2a9
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-01 15:59:27 +0000

    added cacher

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,6 +8,7 @@ import componentSelectContext from './ComponentSwitchContext'
 import MenuBar from './MenuBar'
 //import { ServerProvider } from './ServerContext'
 import useStickyState from './utils/useStickyState';
+import { TopicClientCacher } from './utils/TopicClientCacher'
 
 function App() {
 
@@ -19,21 +20,23 @@ function App() {
   const [url, setUrl] = useStickyState<string | null>(null, "url");
 
   return (
-    <ServerProvider url={url} realServer={false}>
-    <presetButtonsContext.Provider value={value1}> {/* Giving preset button context to children components */}
-      <componentSelectContext.Provider value={value2}> {/* Giving component select context to children components */}
-      <MenuBar setUrl={setUrl}/>
-      <div className="flex h-screen overflow-hidden">
-        <div className="w-1/3 h-5/6 pr-16">
-          <SidePanel />
+    <TopicClientCacher>
+      <ServerProvider url={url} realServer={false}>
+      <presetButtonsContext.Provider value={value1}> {/* Giving preset button context to children components */}
+        <componentSelectContext.Provider value={value2}> {/* Giving component select context to children components */}
+        <MenuBar setUrl={setUrl}/>
+        <div className="flex h-screen overflow-hidden">
+          <div className="w-1/3 h-5/6 pr-16">
+            <SidePanel />
+          </div>
+          <div className="flex w-2/3 h-5/6">
+            <Stage />
+          </div>
         </div>
-        <div className="flex w-2/3 h-5/6">
-          <Stage />
-        </div>
-      </div>
-      </componentSelectContext.Provider>
-    </presetButtonsContext.Provider>
-    </ServerProvider>
+        </componentSelectContext.Provider>
+      </presetButtonsContext.Provider>
+      </ServerProvider>
+    </TopicClientCacher>
   )
 }
 

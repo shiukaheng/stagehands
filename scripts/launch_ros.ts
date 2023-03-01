6afa3ceb4fe2acb65a2ee46efb36a6af2aff7103
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-01 20:36:22 +0000

    ros launch script

---
+++ scripts/launch_ros.ts
@@ -0,0 +1,86 @@
+import chalk = require("chalk");
+import { ChildProcess, spawn } from "child_process";
+import { dockerInstalled, getOS, getWindowsXServer, launchVcxsrv, wslInstalled, wslVersion } from "./lib";
+const art = require('ascii-art');
+
+// Print the ascii art
+art.font('StageHands', 'Doom', function(err, rendered){
+    console.log(
+        chalk.magentaBright.bold(rendered)
+    );
+    console.log(
+        chalk.cyanBright.bold("Checking system requirements...")
+    );
+    const sys = {
+        hasDocker: dockerInstalled(),
+        os: getOS(),
+        wsl: wslVersion(),
+        winX: getWindowsXServer()
+    }
+    console.log(chalk.cyanBright(`Detected OS: ${sys.os}`));
+    console.log(chalk.cyanBright(`Docker installed: ${sys.hasDocker}`));
+    if (sys.os == "win32") {
+        console.log(chalk.cyanBright(`WSL version: ${sys.wsl}`));
+        console.log(chalk.cyanBright(`Windows X Server: ${sys.winX}`));
+    }
+    const dockerSupported = sys.hasDocker
+    const osSupported = ["win32", "linux", "darwin"].includes(sys.os)
+    const wslSupported = !(sys.os == "win32" && sys.wsl == null)
+    if (dockerSupported && osSupported && wslSupported) {
+        console.log(chalk.greenBright.bold("System requirements met!"));
+    } else {
+        console.log(chalk.redBright.bold("System requirements not met!"));
+        if (!dockerSupported) {
+            console.log(chalk.redBright("Docker not installed, or not in PATH"));
+        }
+        if (!osSupported) {
+            console.log(chalk.redBright("Operating system not supported"));
+        }
+        if (!wslSupported) {
+            console.log(chalk.redBright("WSL not installed!"));
+        }
+        // Exit the program
+        process.exit(1);
+    }
+    if (sys.os === "win32" && sys.winX === null) {
+        console.log(chalk.redBright("Warning: Running StageHands on Windows without a X Server detected. GUI applications will not work!"))
+    }
+    if (sys.os === "win32" && sys.winX === "vcxsrv") {
+        console.log(chalk.greenBright("Starting VcXsrv..."));
+        launchVcxsrv();
+    }
+    // If windows and wslg, in workdir ./ros, run ./launch_wslg.bat, pipe output to console
+    let launch: ChildProcess;
+    if (sys.os === "win32" && sys.winX === "wslg") {
+        console.log(chalk.cyanBright("Launching in WSLg mode..."));
+        // Run the windows batch file ./ros/launch_wslg.bat using cmd /c
+        launch = spawn('cmd', ['/c', 'launch_wslg.bat'], { stdio: 'inherit', cwd: './ros' });
+    } else if (sys.os === "win32") {
+        console.log(chalk.cyanBright("Launching in WSL mode..."));
+        // Similar but launch_wsl.bat
+        launch = spawn('cmd', ['/c', 'launch_wsl.bat'], { stdio: 'inherit', cwd: './ros' });
+    } else if (sys.os === "linux") {
+        // Similar but using ./ros/launch_linux.sh and the default shell
+        console.log(chalk.cyanBright("Launching in Linux mode..."));
+        launch = spawn('./ros/launch_linux.sh', { stdio: 'inherit' });
+    } else if (sys.os === "darwin") {
+        console.log(chalk.cyanBright("Launching in macOS mode..."));
+        console.log(chalk.redBright("Warning: macOS does not provide dbus, so some GUI applications like Gazebo may not work!"))
+        launch = spawn('./ros/launch_macos.sh', { stdio: 'inherit' });
+    } else {
+        console.log(chalk.redBright("Unknown operating system!"));
+        process.exit(1);
+    }
+    launch.stdout?.pipe(process.stdout);
+    launch.on('exit', (code) => {
+        // If not 0, exit with code
+        if (code !== 0 && code !== null) {
+            console.log(chalk.redBright.bold(`Docker-compose exited with code ${code}`));
+            process.exit(code);
+        } else {
+            console.log(chalk.greenBright.bold("Docker-compose exited successfully!"));
+            console.log(chalk.greenBright("You can now attach to the StageHands container via the Dev Container extension to develop."));
+            process.exit(0);
+        }
+    });
+});
---
+++ scripts/lib.ts
@@ -0,0 +1,83 @@
+import { execSync } from "child_process";
+
+// Function for checking whether docker is installed
+export function dockerInstalled(): boolean {
+    try {
+        execSync('docker -v', { stdio: 'ignore' });
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+// Function for getting OS
+export function getOS() {
+    return process.platform;
+}
+
+// Function for checking if wsl is installed
+export function wslInstalled(): boolean {
+    try {
+        execSync('wsl -l', { stdio: 'ignore' });
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+// Function for checking if wsl is wsl1 or wsl2
+export function wslVersion(): '1' | '2' | null {
+    // Check if wsl is installed
+    if (!wslInstalled()) {
+        return null;
+    } else {
+        // Check if wsl is wsl1 or wsl2
+        try {
+            execSync('wsl -l -v', { stdio: 'ignore' });
+            return '2';
+        } catch (e) {
+            return '1';
+        }
+    }
+}
+
+// Check if WSLg is installed
+export function wslgInstalled(): boolean {
+    try {
+        execSync('wslg echo', { stdio: 'ignore' });
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+}
+
+// Check if VCXSRV is installed by checking C:\Program Files\VcXsrv\vcxsrv.exe
+export function vcxsrvInstalled(): boolean {
+    try {
+        execSync('C:\\Program Files\\VcXsrv\\vcxsrv.exe -version', { stdio: 'ignore' });
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+// Launch VcXsrv with clipboard support, multiwindow, nowinkill, and no access control
+export function launchVcxsrv() {
+    try {
+        execSync('C:\\Program Files\\VcXsrv\\vcxsrv.exe :0 -clipboard -multiwindow -nowinkill -ac -silent', { stdio: 'ignore' });
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+export function getWindowsXServer(): 'vcxsrv' | 'wslg' | null {
+    if (wslgInstalled()) {
+        return 'wslg';
+    } else if (vcxsrvInstalled()) {
+        return 'vcxsrv';
+    } else {
+        return null;
+    }
+}

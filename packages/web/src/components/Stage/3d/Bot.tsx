f4aea10db9aa8a4382aa771a396b8323dfaccd51
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-12 20:40:31 +0000

    3d cursor test

--- packages/web/src/components/Stage/3d/Bot.tsx
+++ packages/web/src/components/Stage/3d/Bot.tsx
@@ -1,14 +1,17 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useContext, useState } from 'react';
 import { BotState } from 'schema';
 import {Billboard, Text} from '@react-three/drei';
 import { LED } from './LED';
+import { StageContext } from '../Stage';
 
 function Bot({module}: {module: BotState}) {
     const [hovered, hover] = useState(false);
     const [clicked, click] = useState(false);
+    const {setCursor} = useContext(StageContext);
 
     return (
         <Fragment key={module.name}>
+            {/* Label */}
             <group position={module.pose.position as [number, number, number]}>
                 <Billboard position={[0,1.5,0]}>
                     <Text color="white" fontSize={0.5} position={[0, 0, 0]}>
@@ -16,16 +19,23 @@ function Bot({module}: {module: BotState}) {
                     </Text>
                 </Billboard>
             </group>
+            {/* Main model */}
             <mesh
                 castShadow
                 receiveShadow
                 position={module.pose.position as [number, number, number]}
                 scale={clicked ? 1.5 : 1}
                 onClick={(event) => click(clicked)}
-                onPointerOver={(event) => hover(true)}
-                onPointerOut={(event) => hover(false)}>
+                onPointerOver={(event) => {
+                    hover(true);
+                    setCursor('grab');
+                }}
+                onPointerOut={(event) => {
+                    hover(false)
+                    setCursor('default');
+                }}>
                 <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color={hovered ? 'black' : 'darkgray'} />
+                <meshStandardMaterial color={hovered ? 'black' : 'darkgray'}/>
                 <LED ledState={module.ledState.base} position={[0, 0.7, 0]} intensity={5} distance={5} castShadow/>
             </mesh>
         </Fragment>
--- packages/web/src/components/Stage/Stage.tsx
+++ packages/web/src/components/Stage/Stage.tsx
@@ -1,23 +1,41 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { StageScene } from './StageScene'
+import { createContext, useCallback, useRef } from 'react';
+
+export type cursorOverride = 'default' | 'pointer' | 'grab' | 'grabbing';
+export interface IStageContext {
+  setCursor: (cursor: cursorOverride) => void;
+}
+export const StageContext = createContext<IStageContext>({
+  setCursor: () => {}
+});
 
 export function Stage() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const setCursor = useCallback((cursor: cursorOverride) => {
+    if (canvasRef.current && cursor !== null) {
+      canvasRef.current.style.cursor = cursor;
+    }
+  }, [canvasRef]);
   return (
     <div className="flex-grow m-5 ui-shadow ui-div ui-highlight safari-canvas-overflow-fix">
       <Canvas
+        ref={canvasRef}
         shadows
         camera={{
           position: [0, 10, 28],
           fov: 50
         }
         }>
+        <StageContext.Provider value={{ setCursor }}>
         <color attach="background" args={['#222']} />
         <OrbitControls
           minDistance={3}
           maxDistance={30}
         />
         <StageScene />
+        </StageContext.Provider>
       </Canvas>
     </div>
   );

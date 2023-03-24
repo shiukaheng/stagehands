71f932d7ebaa2494b6182dac92a9a29469fa658f
s2078236 <s2078236@ed.ac.uk>
2023-03-24 21:31:08 +0000

    press <- to run the previous preset

--- packages/web/src/components/MenuBar.tsx
+++ packages/web/src/components/MenuBar.tsx
@@ -7,27 +7,29 @@ import componentSelectContext from "../contexts/ComponentSwitchContext";
 import { Preset } from 'schema';
 import { MicPanelSelection, PresetPanelSelection, RunningPresetSelction, SidePanelSelection } from './SidePanel/PresetPanel/PresetPanelSelection';
 
-function handelClickOrKeyDown({componentSelect, setComponentSelect, presets, serviceProvider} : 
+function handelClickOrKeyDown({componentSelect, setComponentSelect, presets, serviceProvider, isRight} : 
     {componentSelect: SidePanelSelection, setComponentSelect: (componentSelect: SidePanelSelection) => void, presets: {id:string,value :Preset}[],
-    serviceProvider: IServiceContext}) {
+    serviceProvider: IServiceContext, isRight: 1|-1}) {
 
     let preset: { id: string, value: Preset } | undefined;
     let presetIndex: number;
-    if (componentSelect.type === "running_preset") {
+    presetIndex = componentSelect.type === "running_preset"? componentSelect.presetIndex + isRight : 0;
 
-        presetIndex = componentSelect.presetIndex + 1;
-    } else {
-        presetIndex = 0;
+    if(presetIndex === -1 ){
+        alert("this is the first preset")
+        return;
     }
-    preset = presets[presetIndex];
-    if (preset === undefined) {
+
+    if (presetIndex === presets.length) {
         setComponentSelect({ type: "preset_panel" } as PresetPanelSelection)
         alert("not preset left to run")
-    } else {
-        console.log(preset)
-        serviceProvider?.runPreset.callback(preset.id);
-        setComponentSelect({ type: "running_preset", presetIndex: presetIndex } as RunningPresetSelction);
-    }
+        return;
+    } 
+    
+    preset = presets[presetIndex];
+    serviceProvider?.runPreset.callback(preset.id);
+    setComponentSelect({ type: "running_preset", presetIndex: presetIndex } as RunningPresetSelction);
+    
 }
 function MenuBar() {
     const serviceProvider = useContext(ServiceContext)!;
@@ -46,11 +48,15 @@ function MenuBar() {
             <button className="bg-red-600 hover:bg-red-800 dark:bg-red-800 dark:hover:bg-red-600 transition-colors duration-500 text-white px-4 py-2 h-full"
                 onKeyDown={(event)=>{
                     if(event.key === "ArrowRight"){
-                        handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider});
+                        handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider,isRight : 1});
+                    }
+                    if(event.key === "ArrowLeft"){
+                        handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider,isRight : -1});
+                    
                     }
                 }}
                 onClick={() => {
-                    handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider});
+                    handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider,isRight : 1});
 
                 }
                 }>run</button>
--- packages/web/src/components/SidePanel/RunningPresetPanel.tsx
+++ packages/web/src/components/SidePanel/RunningPresetPanel.tsx
@@ -9,8 +9,11 @@ export function RunningpresetPanel({presetIndex} : {presetIndex: number}){
     return(
         <div className="flex flex-col gap-5">
             <h1 className="p-5">Running preset : {preset.value.name}</h1>
-            <h1>{"click -> arrow on your keyboard"} </h1>
+            <h1>{"press -> arrow on your keyboard"} </h1>
             <h1> or the run button to run the next preset</h1>
+            <br></br>
+            <h1>{"press <- arrow on your keyboard to run"}</h1>
+            <h1> the previous preset</h1>
         </div>
 
     )

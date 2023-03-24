b81f3a3b4f0af0c0ffa929f51ae848a40840833e
JackLi-hin <s2077992@ed.ac.uk>
2023-03-24 12:21:12 +0000

    patch

--- packages/bridge/src/test/utils/dummyBotPairingServer.ts
+++ packages/bridge/src/test/utils/dummyBotPairingServer.ts
@@ -1,4 +1,4 @@
-import { PairingClient } from "../../../../bot_pairing_client/src";
+import { PairingClient } from "../../../../bot_pairing/src";
 import { Server } from "socket.io";
 import { TopicServer } from "webtopics";
 import { botParingService } from "schema";

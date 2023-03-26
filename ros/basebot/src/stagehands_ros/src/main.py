c513e02ef11410dce5b973f0d02d32f97c58766b
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-26 00:28:57 +0000

    fix

--- ros/basebot/src/stagehands_ros/src/main.py
+++ ros/basebot/src/stagehands_ros/src/main.py
@@ -1,43 +1,58 @@
+import rospy
+import os
+import subprocess
 
 class StagehandsManagementNode:
-    """
-    This is meant to be a ROS node for managing switching between different
-    modes in the StageHands stage automation robot (based on turtlebots):
-    - Pairing mode
-    - Mapping mode
-    - Localization mode
-    - Navigation mode
-    """
     
-        def __init__(self):
-            self._node_name = rospy.get_name()
-            rospy.loginfo("[%s] Initializing " %(self._node_name))
-    
-            # Setup publishers
-            self._pub_mode = rospy.Publisher("~mode", String, queue_size=1)
-    
-            # Setup subscribers
-            self._sub_mode = rospy.Subscriber("~mode", String, self.cbMode, queue_size=1)
-    
-            # Setup services
-            self._srv_mode = rospy.Service("~mode", SetMode, self.srvMode)
-    
-            # Setup parameters
-            self._mode = rospy.get_param("~mode", "pairing")
-    
-            # Setup timers
-            self._timer = rospy.Timer(rospy.Duration(0.1), self.cbTimer)
-    
-            rospy.loginfo("[%s] Initialized " %(self._node_name))
-    
-        def cbMode(self, msg_mode):
-            self._mode = msg_mode.data
-    
-        def srvMode(self, req_mode):
-            self._mode = req_mode.mode
-            return SetModeResponse(True)
-    
-        def cbTimer(self, event):
-            msg_mode = String()
-            msg_mode.data = self._mode
-            self._pub_mode.publish(msg_mode)
+    def __init__(self):
+        self._node_name = rospy.get_name()
+        self._name = None
+        self.get_name()
+        rospy.loginfo("[%s] Initializing " %(self._node_name))
+
+        # # Setup publishers
+        # self._pub_mode = rospy.Publisher("~mode", String, queue_size=1)
+
+        # # Setup subscribers
+        # self._sub_mode = rospy.Subscriber("~mode", String, self.cbMode, queue_size=1)
+
+        # # Setup services
+        # self._srv_mode = rospy.Service("~mode", SetMode, self.srvMode)
+
+        # # Setup parameters
+        # self._mode = rospy.get_param("~mode", "pairing")
+
+        # # Setup timers
+        # self._timer = rospy.Timer(rospy.Duration(0.1), self.cbTimer) 
+        rospy.loginfo("[%s] Launching bot: [%s]" %(self._node_name, self._name))
+        rospy.loginfo("[%s] Initialized " %(self._node_name))
+
+        self.start_pairing()
+
+    def get_name(self):
+        """
+        Get the name of the bot
+        """
+        # Could be substituted with a ROS service call
+
+        # Run "node --experimental-specifier-resolution=node --loader ts-node/esm /catkin_ws/src/stagehands-js/packages/utils/dist/getName.js" and cache the result
+        if self._name is None:
+            # Make it run in /catkin_ws/src/stagehands-js/packages/
+            # Get original working directory
+            original_wd = os.getcwd()
+            os.chdir("/catkin_ws/src/stagehands-js/packages/")
+            self._name = subprocess.check_output(["node", "--experimental-specifier-resolution=node", "--loader", "ts-node/esm", "/catkin_ws/src/stagehands-js/packages/utils/dist/getName.js"]).decode("utf-8").strip()
+            # Restore original working directory
+            os.chdir(original_wd)
+            return self._name
+        else:
+            return self._name
+
+    def start_pairing():
+        
+
+
+if __name__ == '__main__':
+    rospy.init_node('stagehands_management_node', anonymous=False)
+    stagehands_management_node = StagehandsManagementNode()
+    rospy.spin()

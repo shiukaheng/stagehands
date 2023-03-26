1ddc7217051c3e6bb96f38aa160f35de3e91e131
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-26 19:41:53 +0100

    trying to improve build flow

---
+++ packages/bot/webpack.config.cjs
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts', // Your entry file
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  externalsPresets: { node: true },
+};

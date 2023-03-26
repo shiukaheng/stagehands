30fc106726760ff6ad838af6a3c933c0f9296fcb
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-26 03:44:07 +0100

    prebuild

---
+++ ros/basebot/scripts/prebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. init.sh
+cd /catkin_ws/
+build
---
+++ ros/prebuild.sh
@@ -0,0 +1,1 @@
+docker-compose -f ./docker-compose.yaml -f ./docker-compose.prod.yaml -f ./docker-compose.useprebuild.yaml -f ./docker-compose.prebuild.yaml up

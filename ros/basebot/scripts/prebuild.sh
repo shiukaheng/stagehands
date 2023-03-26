d4c79703e0460aa6b016f0766d25cbf54a6f8b7f
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-26 22:56:31 +0100

    somewhat final change on prebuilding / interface

--- ros/basebot/scripts/prebuild.sh
+++ ros/basebot/scripts/prebuild.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 . init.sh
-cd /catkin_ws/src/stagehands-js
-# npm install
-# npm run build-bot-deps
+cd /catkin_ws/src/stagehands-js/
+npm install
+npm run build-bot-deps
+cd /catkin_ws/src/stagehands-js/packages/bot/
+npm run build
 cd /catkin_ws/
 build

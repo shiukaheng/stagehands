6fce2c76572acc44993f16067c42fae24d191ed8
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-27 06:12:14 +0100

    prod image changes

--- ros/update_prod.bat
+++ ros/update_prod.bat
@@ -1,2 +1,1 @@
-docker build --platform linux/arm64 -t shiukaheng/stagehands:prod -f ./basebot/Dockerfile ../
-docker push shiukaheng/stagehands:prod
+docker build --platform linux/arm64 -t shiukaheng/stagehands:prod -f ./basebot/Dockerfile ../ && docker push shiukaheng/stagehands:prod

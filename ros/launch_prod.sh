571c9debf5e68cf2ecf4789a38c04e278df66123
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-27 21:33:03 +0100

    Update launch_prod.sh

--- ros/launch_prod.sh
+++ ros/launch_prod.sh
@@ -1,7 +1,7 @@
 function launch() {
     local current_dir=$(pwd)
     cd /home/pi/stagehands/ros
-    sudo docker-compose -f ./docker-compose.yaml -f ./docker-compose.prod.yaml -f ./docker-compose.rpi.yaml up
+    sudo docker-compose -f ./docker-compose.yaml -f ./docker-compose.prod.yaml -f ./docker-compose.dev.yaml -f ./docker-compose.rpi.yaml up
     cd "$current_dir"
 }
 

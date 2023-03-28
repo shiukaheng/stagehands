89bbd634ff4581b0835d25daf5a0731ae85fd820
NoahRamsden <s2059564@ed.ac.uk>
2023-03-28 15:30:30 +0100

    change

--- packages/web/src/components/ConnectionScreen/AvailableBotWidget.tsx
+++ packages/web/src/components/ConnectionScreen/AvailableBotWidget.tsx
@@ -9,16 +9,31 @@ export function AvailableBotWidget({ botStatus, botID }: { botStatus: string, bo
     {/* TODO */}
     {/* want know if the bot is connected or not so we can highlight it red or green (or have some indication of its connection status) */}
     {/* also so if its connected we can call the disconnect service and vice-versa */}
+    if (botStatus === "connected") {
     return (
         <button
-                    id={"nameOfBot"}
-                    className="ui-shadow ui-highlight ui-div font-bold box-border h-20 w-64 rounded m-2 px-8" // Could change colour here depending on connection status
-                    onClick={() => { console.log("Connection button clicked")
-                    {serviceProvider?.connectBot.callback(botID)}}} 
+                    id={"connectedBot"}
+                    className="ui-shadow ui-highlight ui-div font-bold box-border h-20 w-64 rounded m-2 px-8 bg-green-500" // Could change colour here depending on connection status
+                    onClick={() => { console.log("Connection button clicked to disconnect")
+                    {serviceProvider?.disConnectBot.callback(botID)}}} 
                     onMouseEnter={() => { 
                         setBotID(botStatus)
                         console.log("Mouse hovering!") }}> {/* onClick to connect? */}
                     {botID} - {botStatus}{/*{botState.name} - {botState.status}/disconnected*/}
                 </button>
     )
+    } else if (botStatus === "disconnected") {
+            return (
+                <button
+                    id={"disconnectedBot"}
+                    className="ui-shadow ui-highlight ui-div font-bold box-border h-20 w-64 rounded m-2 px-8 bg-red-900" // Could change colour here depending on connection status
+                    onClick={() => { console.log("Connection button clicked to connect")
+                    {serviceProvider?.connectBot.callback(botID)}}} 
+                    onMouseEnter={() => { 
+                        setBotID(botStatus)
+                        console.log("Mouse hovering!") }}> {/* onClick to connect? */}
+                    {botID} - {botStatus}{/*{botState.name} - {botState.status}/disconnected*/}
+                </button>
+            )
+    }
 }

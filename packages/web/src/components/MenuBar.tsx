ac9e3830da6076a3af2ac201c51c5fdffa7080a6
s2078236 <s2078236@ed.ac.uk>
2023-03-28 21:39:14 +0100

    update layout

--- packages/web/src/components/MenuBar.tsx
+++ packages/web/src/components/MenuBar.tsx
@@ -7,6 +7,9 @@ import componentSelectContext from "../contexts/ComponentSwitchContext";
 import { Preset } from 'schema';
 import { MicPanelSelection, PresetPanelSelection, RunningPresetSelction, SidePanelSelection } from './SidePanel/PresetPanel/PresetPanelSelection';
 import { handelClickOrKeyDown } from '../utils/handelKeyDown';
+import { Tooltip } from "react-tooltip"
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { BsFillExclamationTriangleFill } from 'react-icons/bs';
 
 
 function MenuBar() {
@@ -23,29 +26,44 @@ function MenuBar() {
                 Stagehands Console
             </div>
 
-            <button className="bg-red-600 hover:bg-red-800 dark:bg-red-800 dark:hover:bg-red-600 transition-colors duration-500 text-white px-4 py-2 h-full"
-                onKeyDown={(event)=>{
-                    if(event.key === "ArrowRight"){
-                        handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider,isRight : 1});
+            <button className="px-4 py-2 h-full rounded ui-hover-highlight"
+                onKeyDown={(event) => {
+                    if (event.key === "ArrowRight") {
+                        handelClickOrKeyDown({ componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider, isRight: 1 });
                     }
-                    if(event.key === "ArrowLeft"){
-                        handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider,isRight : -1});
-                    
+                    if (event.key === "ArrowLeft") {
+                        handelClickOrKeyDown({ componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider, isRight: -1 });
+                    }
+                    if (event.key === "Escape") {
+                        setComponentSelect({ type: "mic_panel", presetID: null })
                     }
                 }}
                 onClick={() => {
-                    handelClickOrKeyDown({componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider,isRight : 1});
-
+                    handelClickOrKeyDown({ componentSelect, setComponentSelect, presets: provider?.stage?.presets!, serviceProvider, isRight: 1 });
                 }
-                }>run</button>
-
-            <button onClick={() => {
-                serviceProvider?.emergencyStop.callback();
-                let resume = confirm("Resume?");
-                if (resume) {
-                    serviceProvider?.emergencyStopClear.callback();
                 }
-            }} className="bg-red-600 hover:bg-red-800 dark:bg-red-800 dark:hover:bg-red-600 transition-colors duration-500 text-white px-4 py-2 h-full">Stop</button>
+                data-tooltip-id={"run-from-the-start-button"}
+                data-tooltip-content="execute presets from the start"
+                data-tooltip-place='left'>
+                <AiFillPlayCircle size={20} />
+                <Tooltip id={"run-from-the-start-button"} />
+            </button>
+
+            <button
+                className="bg-red-600 hover:bg-red-800 dark:bg-red-800 dark:hover:bg-red-600 transition-colors duration-500 text-white px-4 py-2 h-full"
+                onClick={() => {
+                    serviceProvider?.emergencyStop.callback();
+                    let resume = confirm("Resume?");
+                    if (resume) {
+                        serviceProvider?.emergencyStopClear.callback();
+                    }
+                }}
+                data-tooltip-id={"Emergency Stop Button!!"}
+                data-tooltip-content="Emergency Stop Button!!"
+                data-tooltip-place='left'>
+                <BsFillExclamationTriangleFill size={20} />
+                <Tooltip id={"Emergency Stop Button!!"} />
+            </button>
         </div>
     );
 }

6406984a86d90acc15287bc583ddcb780d502978
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-04-01 18:58:09 +0100

    threaded led

--- magic/led.py
+++ magic/led.py
@@ -2,8 +2,12 @@
 
 __all__ = ['GroveWS2813RgbStrip', 'PixelStrip', 'Color']
 
+import math
 import time
 from rpi_ws281x import PixelStrip, Color
+import threading
+
+from dataclasses import dataclass
 
 # LED strip configuration
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -48,10 +52,80 @@ class GroveWS2813RgbStrip(PixelStrip):
         for u in ([num for num in range(6, 14)] + [num for num in range(23, 30)]):
             self.setPixelColor(u, color)
         self.show()
+        
+@dataclass
+class LEDState:
+    # Base color (r,g,b):
+    baseColor: tuple
+    # Flashing (True/False)
+    flashing: bool
+    # Flashing frequency (Hz) or None if not flashing
+    modulation_freq: float
 
 class LED():
     def __init__(self):
         self.strip = GroveWS2813RgbStrip(12, 30)
+        self.frameRate = 30
+        self.phase = 0
+        self.modulation_freq = 0.5
+        self.baseColor = (0,0,0)
+        self.flashing = False
+        self.running = True
+        
+        self.ledState = LEDState((0,0,0), False, 1)
+        self.overrideLEDState = None # Used for notifications
 
-    def set(self,r,g,b):
+
+    def _set(self,r,g,b):
         self.strip.setColor(Color(r,g,b))
+
+    def _update(self, dt):
+        # Update phase (0 - 2 * pi)
+        self.phase += 2 * math.pi * self.modulation_freq * dt
+        if self.phase > 2 * math.pi:
+            self.phase -= 2 * math.pi
+
+        # Calculate color
+        color = self.baseColor
+        if self.flashing:
+            color = [int(c * (1 + math.sin(self.phase))) for c in color]
+
+        # Set color
+        self._set(*color)
+
+    def _loop(self):
+        while self.running:
+            # Update
+            self._update(1.0 / self.frameRate)
+
+            # Sleep
+            time.sleep(1.0 / self.frameRate)
+
+    def _setRawLEDState(self, ledState):
+        # Update / call the relevant functions
+        self.baseColor = ledState.baseColor
+        self.flashing = ledState.flashing
+        self.modulation_freq = ledState.modulation_freq if ledState.flashing else 0
+
+    # Below are the functions you ACTUALLY should care about
+
+    def start(self):
+        self.thread = threading.Thread(target=self._loop)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def stop(self):
+        self.running = False
+        self.thread.join()
+
+    def setLEDState(self, ledState):
+        self.ledState = ledState
+        self._setRawLEDState(ledState)
+
+    def setOverrideLEDState(self, ledState):
+        self.overrideLEDState = ledState
+        self._setRawLEDState(ledState)
+
+    def clearOverrideLEDState(self):
+        self.overrideLEDState = None
+        self._setRawLEDState(self.ledState)

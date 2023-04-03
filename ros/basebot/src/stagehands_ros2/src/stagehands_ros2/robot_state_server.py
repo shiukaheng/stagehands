425b97a3816fa3d3835ae1ff9d3e25b1931d54ab
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-04-03 17:09:27 +0100

    debug stuff

--- ros/basebot/src/stagehands_ros2/src/stagehands_ros2/robot_state_server.py
+++ ros/basebot/src/stagehands_ros2/src/stagehands_ros2/robot_state_server.py
@@ -13,24 +13,30 @@ class RobotStateServer:
 
     def __init__(self):
         # Initialize node
+        rospy.loginfo("Initializing robot state server")
         rospy.init_node('robot_state_server')
 
         # Initialize publishers
+        rospy.loginfo("Initializing publishers")
         self.feedbackPublisher = rospy.Publisher('stagehands_feedback_state', StagehandsFeedbackState, queue_size = 10)
         self.transformListener = tf.TransformListener()
         
         # Initialize mic module client
+        rospy.loginfo("Initializing mic module client")
         self.micModuleClient = MicModule("/dev/ttyACM0", 115200)
         self.micModuleClient.start()
 
         # Initialize LED client
+        rospy.loginfo("Initializing LED client")
         self.ledClient = LED()
         self.ledClient.start()
 
         # Initialize navigation client
+        rospy.loginfo("Initializing navigation client")
         self.navClient = NavClient()
 
         # Initialize state subscriber
+        rospy.loginfo("Initializing state subscriber")
         rospy.Subscriber("stagehands_command_state", StagehandsCommandState, self.onReceiveState)
 
     def startPublishState(self):
@@ -41,6 +47,8 @@ class RobotStateServer:
         - Live navigation data (where the bot is in the world)
         - Live mic module data (sensed height and angle)
         """
+
+        rospy.loginfo("Starting state publisher")
 
         # Create a clock / loop
         rate = rospy.Rate(30)
@@ -80,12 +88,14 @@ class RobotStateServer:
         - Mic module state -> set mic height and angle
         - Navigation state -> set target pose
         """
+        rospy.loginfo("Received state: " + str(state))
 
         # Recieves a stagehandsState msg (identical to original request, just with unused return string removed)
         self.ledClient.setLEDState((state.ledRGBColour[0], state.ledRGBColour[1], state.ledRGBColour[2]), state.isFlashing, state.flashFrequency)
         # self.micModuleClient.setState(state.micHeight, state.micAngle)
 
     def start(self):
+        rospy.loginfo("Starting robot state server")
         # Start the publish state thread
         publishStateThread = threading.Thread(target=self.startPublishState)
         publishStateThread.start()
